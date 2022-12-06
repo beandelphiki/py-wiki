6fe3c184d4c3a12b467fbb43bfe2af4494bc2f4e
Orlando Medina <orlando@medinalabs.com>
2022-12-06 12:49:21 -0600

    feat(create-api): This sets up the tests and the method for access.

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import requests
-import jsonify
+import json
 
 #This is just a set of test end points so I can validate things look good and that the install looks correct.
 
@@ -15,3 +15,26 @@ def extractFromURL():
     x = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=Yoshua_Bengio&rvlimit=1&formatver sion=2&format=json&rvprop=content")
     result = str(x.status_code)
     return x.json()
+
+@app.route("/testExtract")
+def extractKeyInfo():
+    x = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=Yoshua_Bengio&rvlimit=1&formatver sion=2&format=json&rvprop=content")
+    result = str(x.status_code)
+    jsonExtract = x.json()
+    #print (jsonExtract['query']['pages']['47749536'])
+
+
+    #first element in the embedded message from Wikipedia -- We need to know the page id to get to the entry point of the rest of the data
+    firstElem = list(jsonExtract['query']['pages'].keys())[0]
+
+    titleName = jsonExtract['query']['pages'][firstElem]['title']
+
+    content = jsonExtract['query']['pages'][firstElem]['revisions'][0]['*']
+
+
+    
+    print(firstElem)
+    print(titleName)
+    print(content)
+    return jsonExtract
+
