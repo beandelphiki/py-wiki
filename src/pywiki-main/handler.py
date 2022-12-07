e8afaf3fde98e1539be17edbe71ef5002744aeb0
Orlando Medina <orlando@medinalabs.com>
2022-12-07 12:30:15 -0600

    feat(create-api): This sets up the basic functionality of the api call minus the rules check.

--- src/pywiki-main/handler.py
+++ src/pywiki-main/handler.py
@@ -1,6 +1,63 @@
 from flask import Flask
+import requests
+import json
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+
+
+@app.route("/<title>/countryCode/<countryCode>/descriptor/<descriptor>")
+def extractDescription( title:str, countryCode:str, descriptor:str ):
+    url = "https://"+countryCode+".wikipedia.org/w/api.php"
+    params={
+        'action':'query',
+        'prop':'revisions',
+        'titles': title,
+        'rvlimit':'1',
+        'formatversion':'2',
+        'format': 'json',
+        'rvprop':'content'
+    }
+
+    x = requests.get(url,params)
+
+    jsonExtract = x.json()
+
+    print (type(jsonExtract))
+    #Run our rules
+
+    #Check to make sure the ShortDescription is there
+
+
+    #Check to make sure the Article is there
+
+
+    #grab the content from the page id
+    content = jsonExtract['query']['pages'][0]['revisions'][0]['content']
+
+    #Clean up the Short Description
+    DescriptorSanitized = descriptor.replace('_', ' ')
+    finalDescriptor = "{{"+ DescriptorSanitized +"|"
+
+
+    #grab the location of the short description
+    findFirstOccurance = content.find("}}")
+    finalString = content[content.find(finalDescriptor)+len(finalDescriptor):findFirstOccurance]
+    
+    #Sanitize the output just in case a spare | shows up
+    shortDescSanitized = finalString.replace('|','')
+
+    return shortDescSanitized 
+
+
+
+
+
+    def ruleArticleIsMissing(inputJsonObj:dict):
+        return 0
+
+    def ruleNoShortDescription():
+        return 0
+
+    
